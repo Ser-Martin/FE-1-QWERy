236e2fd64769b8d0ce47485bd74da07c8bf103b3
Sergiy Martynov <martins.codespace.kh@gmail.com>
2023-01-05 13:03:52 +0200

    refactor: change projects basic structure

---
+++ old/js/2nd-block-slider.js
@@ -0,0 +1,13 @@
+$('.slider-row').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerMode: false,
+  variableWidth: true,
+  adaptiveHeight: true,
+  arrows: true,
+  prevArrow: $('#slick-prev'),
+  nextArrow: $('#slick-next'),
+});
